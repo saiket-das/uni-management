e3e5dad079974d940959485c2e3dbcade781896d
saiket-das <saiketdas02@gmail.com>
2024-07-01 05:36:02 +0600

    Create admin routes & dashboard sidebar

---
+++ src/pages/admin/CreateStudent.tsx
@@ -0,0 +1,9 @@
+const CreateStudent = () => {
+  return (
+    <div>
+      <h1>Create a new student</h1>
+    </div>
+  );
+};
+
+export default CreateStudent;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import { adminRoutes } from "./admin.routes";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+
+  // Admin routes
+  {
+    path: "/admin",
+    element: <App />,
+    children: adminRoutes,
+  },
+
+  // Authentication
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
