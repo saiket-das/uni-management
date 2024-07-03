7c51ab9d60cde66d80d12f5e865cc90553cf783e
saiket-das <saiketdas02@gmail.com>
2024-07-03 22:34:07 +0600

    Set cookies in the browser

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,40 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
 const Login = () => {
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      id: "A-0001",
+      password: "securepassword123",
+    },
+  });
+  const [login, { data: response, error }] = useLoginMutation();
+
+  console.log("Response: ", response);
+  console.log("Error: ", error);
+
+  // Submit data to login
+  const onSubmit = (data) => {
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    };
+    console.log(userInfo);
+    login(userInfo);
+  };
+
   return (
-    <div>
-      <h1>Login page</h1>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">User Id</label>
+        <input type="text" {...register("id")} />
+
+        <label htmlFor="pasword">Password</label>
+        <input type="text" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
   );
 };
 
