3d594492d090e9865899e7acb9c872bbb6a9cfdd
saiket-das <saiketdas02@gmail.com>
2024-07-06 03:55:06 +0600

    Setting up base query to send Access Token with each request

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,18 +1,14 @@
 import { Button } from "antd";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
-import { setUser } from "../redux/features/auth/authSlice";
+import { setUser, UserProps } from "../redux/features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
-import { Navigate } from "react-router-dom";
-
-type LoginProps = {
-  id: string;
-  password: string;
-};
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const Login = () => {
-  const { register, handleSubmit } = useForm<LoginProps>({
+  const { register, handleSubmit } = useForm({
     defaultValues: {
       id: "A-0001",
       password: "securepassword123",
@@ -20,27 +16,33 @@ const Login = () => {
   });
   const [login] = useLoginMutation();
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   // Submit data to login
-  const onSubmit = async (data: LoginProps) => {
-    const userInfo = {
-      id: data.id,
-      password: data.password,
-    };
-    const res = await login(userInfo).unwrap();
-    const token = res.data.accessToken;
+  const onSubmit = async (data: FieldValues) => {
+    const toastId = toast.loading("Loggin in");
+    try {
+      const userInfo = {
+        id: data.id,
+        password: data.password,
+      };
+      const res = await login(userInfo).unwrap();
 
-    // Decode token & set user info and user token in local storage
-    const user = verifyToken(token);
+      // Decode token & set user info and user token in local storage
+      const token = res.data.accessToken;
+      const user = verifyToken(token) as UserProps;
 
-    dispatch(
-      setUser({
-        user: user,
-        token: res.data.accessToken,
-      })
-    );
-    console.log(user);
-    <Navigate to={`/dmin/dashboard`} replace={true} />;
+      dispatch(
+        setUser({
+          user: user,
+          token: res.data.accessToken,
+        })
+      );
+      toast.success("Login Successfully!", { id: toastId, duration: 2000 });
+      navigate(`/${user.role}/dashboard`);
+    } catch (error) {
+      toast(error.message, { id: toastId });
+    }
   };
 
   return (
--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -2,12 +2,23 @@ import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateStudent from "../pages/admin/CreateStudent";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
+import AcademicSemester from "../pages/admin/academicManagement/AcademicSemester";
 
 export const adminPaths = [
   {
     name: "Dashboard",
     path: "dashboard",
     element: <AdminDashboard />,
+  },
+  {
+    name: "Academic Management",
+    children: [
+      {
+        name: "Academic Semester",
+        path: "academic-semester",
+        element: <AcademicSemester />,
+      },
+    ],
   },
   {
     name: "User Management",
@@ -21,16 +32,6 @@ export const adminPaths = [
         name: "Create Faculty",
         path: "create-faculty",
         element: <CreateFaculty />,
-      },
-      {
-        name: "Create Student",
-        path: "create-student",
-        element: <CreateStudent />,
-      },
-      {
-        name: "Create Student",
-        path: "create-student",
-        element: <CreateStudent />,
       },
       {
         name: "Create Student",
