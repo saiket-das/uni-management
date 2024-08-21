a70327a0447c1a64c1c803f49dad539d44db01b2
saiket-das <saiketdas02@gmail.com>
2024-08-21 18:28:02 +0530

    Add icons to the sidebar

--- src/routes/faculty.routes.tsx
+++ src/routes/faculty.routes.tsx
@@ -1,15 +1,19 @@
+import { BookOpen, LayoutDashboard } from "lucide-react";
 import { ROUTES } from "../constants/route";
 import FacultyDashboard from "../pages/faculty/FacultyDashboard";
+import MyCourses from "../pages/faculty/MyCourses";
 
 export const facultyPaths = [
   {
     name: "Dashboard",
     path: ROUTES.dashboard,
     element: <FacultyDashboard />,
+    icon: <LayoutDashboard size={16} />,
   },
   {
-    name: "Offered Course",
-    path: ROUTES.offered_course,
-    element: <FacultyDashboard />,
+    name: "My Courses",
+    path: ROUTES.my_course,
+    element: <MyCourses />,
+    icon: <BookOpen size={16} />,
   },
 ];
